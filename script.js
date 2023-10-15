978ffb40cded1cadb533c0d90d53f200c6335e03
Keanna Jordan <kcheaves@hotmail.com>
2023-10-14 21:52:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let name = document.getElementById("state");
+let newName = prompt("What state are you from?");
+
+let age = document.getElementById("age");
+let newage = prompt("How old are you?");
+
+name.innerHTML = state;
+
+var button = document.getElementById ("btn");
+button.addEventListener (click, function () {
+  alert ("Click to to move to the next page")
+});
